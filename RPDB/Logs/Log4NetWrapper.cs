aa403a4628b2ae767154585cc6a12d1aa1b257ca
dmva <dmva@ua.fm>
2021-03-05 17:32:19 +0200

    add logs

---
+++ RPDB/Logs/Log4NetWrapper.cs
@@ -0,0 +1,70 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPDB.Logs
+{
+    public class Log4NetWrapper : ILogger
+    {
+        private readonly log4net.ILog _logger;
+
+        public Log4NetWrapper()
+        {
+            _logger = LogManager.GetLogger("LOGGER");
+            InitLogger();
+        }
+
+        private static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+        #region ILogger Members
+
+        public void Log(LogLevel level, string message)
+        {
+            if (_logger == null)
+                return;
+
+            var logger = _logger.Logger;
+
+            logger.Log(logger.GetType(), ToLog4NetLevel(level), message, null);
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            if (_logger == null)
+                return;
+
+            _logger.Error(message, ex);
+        }
+
+        #endregion
+
+        private static log4net.Core.Level ToLog4NetLevel(LogLevel level)
+        {
+            switch (level)
+            {
+                case LogLevel.Debug:
+                    return log4net.Core.Level.Debug;
+                case LogLevel.Info:
+                    return log4net.Core.Level.Info;
+                case LogLevel.Error:
+                    return log4net.Core.Level.Error;
+            }
+
+            // Null should never happen. Exception will raise in switch if do not any value is valid.
+            return null;
+        }
+
+        #region ILogger Members
+
+
+
+
+        #endregion
+    }
+}
---
+++ RPDB/Logs/LogLevel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPDB.Logs
+{
+    public enum LogLevel
+    {
+        /// <summary>
+        /// Not visibile messages in production.
+        /// </summary>
+        Debug,
+        /// <summary>
+        /// Visible messages in production, but not errors.
+        /// </summary>
+        Info,
+        /// <summary>
+        /// Errors, visible in production.
+        /// </summary>
+        Error
+    }
+}
